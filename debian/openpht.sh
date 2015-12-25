fc84b9043cddbc337eadc21375d700e13d5b3881
Norbert Preining <norbert@preining.info>
2015-12-25 10:16:05 +0900

    add debian packaging rebranded to openpht

---
+++ debian/openpht.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+PHTROOT=/usr/lib/openpht
+
+XBMC_HOME=$PHTROOT/share/XBMC
+export XBMC_HOME
+#Use export AE_ENGINE=SOFT to disable pulse audio
+#export AE_ENGINE=SOFT
+$PHTROOT/bin/plexhometheater
+
